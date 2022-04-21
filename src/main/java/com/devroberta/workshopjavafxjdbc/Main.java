ebfdb7dd65f7af96a1860ab8d38d58618b07d864
Roberta de Siqueira <designer.roberta@gmail.com>
2022-04-21 13:01:16 -0300

    Main View

---
+++ src/main/java/com/devroberta/workshopjavafxjdbc/Main.java
@@ -0,0 +1,30 @@
+
+package com.devroberta.workshopjavafxjdbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+            Parent parent = fxmlLoader.load();
+            Scene scene = new Scene(parent);
+            stage.setTitle("Sample JavaFX application");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
