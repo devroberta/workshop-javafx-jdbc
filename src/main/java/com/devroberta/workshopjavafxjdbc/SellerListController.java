7949371a0742fd428cb9110b0ab81dceb2bda78e
Roberta de Siqueira <designer.roberta@gmail.com>
2022-04-23 17:33:49 -0300

    Seller TableView

--- src/main/java/com/devroberta/workshopjavafxjdbc/SellerListController.java
+++ src/main/java/com/devroberta/workshopjavafxjdbc/SellerListController.java
@@ -18,6 +18,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -32,6 +33,12 @@ public class SellerListController implements Initializable, DataChangeListener {
     private TableColumn<Seller, Integer> tableColumnId;
     @FXML
     private TableColumn<Seller, String> tableColumnName;
+    @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
     @FXML
     private TableColumn<Seller, Seller> tableColumnEDIT;
     @FXML
@@ -61,6 +68,11 @@ public class SellerListController implements Initializable, DataChangeListener {
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory((new PropertyValueFactory<>("name")));
+        tableColumnEmail.setCellValueFactory((new PropertyValueFactory<>("email")));
+        tableColumnBirthDate.setCellValueFactory((new PropertyValueFactory<>("birthDate")));
+        Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory((new PropertyValueFactory<>("baseSalary")));
+        Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
         Stage stage = (Stage) Main.getScene().getWindow();
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
